e2d95bb33029218274b96f8a8be451caba40b1c7
joostburgers <joostburgers@gmail.com>
2022-08-19 22:50:40 -0400

    Add sunburst data

--- js/character_charts.js
+++ js/character_charts.js
@@ -28,7 +28,7 @@ $(function () {
     });
     var contextScrollychart1 = document.getElementById('scrollychart1').getContext("2d");
     var contextScrollychart2 = document.getElementById('scrollychart2').getContext("2d");
-	var contextBar = document.getElementById('barchart').getContext("2d");
+	//var contextBar = document.getElementById('barchart').getContext("2d");
 	
 
     // examine example_data.json for expected response data
@@ -74,37 +74,38 @@ $(function () {
 
     });
 
-	var barchart1 = new Chart(contextBar, {
-        type: 'bar',
-        data: {
-            labels: ["Moby Dick (H. Melville)", "Huckleberry Finn (M. Twain)", "The Great Gatsby (F.S. Fitzgerald)", "Absalom, Absalom! (W. Faulker)", "Beloved (T. Morrison)"],
-            datasets: [{
-                data: [22, 19, 14, 50, 12],
-                backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.6)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-                borderColor: DefaultborderColor,
-                borderWidth: DefaultborderWidth
-            }]
+	//var barchart1 = new Chart(contextBar, {
+ //       type: 'bar',
+ //       data: {
+ //           labels: ["Moby Dick (H. Melville)", "Huckleberry Finn (M. Twain)", "The Great Gatsby (F.S. Fitzgerald)", "Absalom, Absalom! (W. Faulker)", "Beloved (T. Morrison)"],
+ //           datasets: [{
+ //               data: [22, 19, 14, 50, 12],
+ //               backgroundColor: [
+	//				'rgba(255, 99, 132, 0.2)',
+	//				'rgba(54, 162, 235, 0.2)',
+	//				'rgba(255, 206, 86, 0.2)',
+	//				'rgba(75, 192, 192, 0.6)',
+	//				'rgba(153, 102, 255, 0.2)',
+	//				'rgba(255, 159, 64, 0.2)'
+	//			],
+ //               borderColor: DefaultborderColor,
+ //               borderWidth: DefaultborderWidth
+ //           }]
 
-        },
-        options: {
-		responsive: true,
-            legend: {
-                display: false
-            },
-			scales: {
-				y: {
-					beginAtZero: true,
-				}
-			},
+ //       },
+ //       options: {
+	//	responsive: true,
+ //           legend: {
+ //               display: false
+ //           },
+	//		scales: {
+	//			y: {
+	//				beginAtZero: true,
+	//			}
+	//		},
 
-	}});
+	//	}
+	//});
 
 
     ajax_chart(scrollychart1, demography_chart_url);
