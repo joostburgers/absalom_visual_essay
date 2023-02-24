69deba4b854aa5c2369d181349215168f316dc32
joostburgers <joostburgers@gmail.com>
2023-02-24 15:18:39 -0500

    Plot chart data

--- js/event_charts.js
+++ js/event_charts.js
@@ -285,6 +285,185 @@ function handleStepEnter(response) {
 
 }
 
+function makeplotAA() {
+    var data;
+    $.ajax({
+        type: "GET",
+        url: "https://raw.githubusercontent.com/arundhatibala/absalom/main/data/plot_structure_absalom.csv",
+        dataType: "text",
+        success: function (response) {
+            data = $.csv.toObjects(response, { headers: true });
+            processDataAA(data);
+        }
+    });
+};
+
+
+function processDataAA(allRows) {
+
+    console.log(allRows);
+    console.log(allRows.length)
+    var chrono = []
+    var value = []
+    var series = []
+    var text = [];
+    var xoffset = [];
+    var yoffset = [];
+
+    for (var i = 0; i < allRows.length; i++) {
+        row = allRows[i];
+        chrono.push(parseInt(row['Chrono']));
+        value.push(parseInt(row['value']));
+        series.push(row['series']);
+        text.push(row['name']);
+        xoffset.push(row['xoffset']);
+        yoffset.push(row['yoffset']);
+
+    }
+    console.log("chrono =", chrono, "value = ", value, "series = ", series, "xoffset = ", xoffset, "yoffset = ", yoffset);
+    makePlotly(chrono, value, series, text, xoffset, yoffset);
+}
+
+function makePlotly(chrono, value, series, text, xoffset, yoffset) {
+
+    //Create two different data sets for the frames
+    y = chrono.filter((v, i) => i % 2) //This is the default y variable
+
+    //x values switch
+    plot_value = value.filter((v, i) => !(i % 2))
+    story_value = value.filter((v, i) => i % 2)
+
+    //text values switch
+    plot_text = text.filter((v, i) => i % 2)
+    story_text = text.filter((v, i) => !(i % 2))
+
+
+    console.log(y, plot_value, story_value)
+
+    //make helper functions to simplify this
+
+    var data = [{
+        type: "scatter",
+        mode: "markers+text",
+        x: plot_value,
+        y: y,
+        name: series[0],
+        text: plot_text,
+        textposition: ['top right',
+            'bottom left',
+            'top left'
+        ]
+
+    }];
+
+    var frames = [
+        {
+            name: series[0],
+            data: [{
+                x: plot_value,
+                textposition: ['top right',
+                    'bottom left',
+                    'top left'
+                ]
+            }]
+        },
+        {
+            name: series[1],
+            data: [{
+                x: story_value,
+                textposition: ['top right',
+                    'bottom left',
+                    'top right'
+                ]
+            }]
+        }
+    ]
+
+
+    var layout = {
+        title: { text: "<b>Plot Structure Chart</b> <br>Major Events in Sutpen's Life" },
+        showlegend: false,
+        xaxis: {
+            showgrid: false,
+            title: { text: "Chapter" },
+            autotick: false,
+            tickmode: 'array',
+            tickvals: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            range: [0.5, 9]
+        },
+        yaxis: {
+            title: { text: "Chronology" },
+            showgrid: false,
+            tickmode: 'array',
+            tickvals: [1, 2, 3],
+            zeroline: false
+        },
+
+        sliders: [{
+            pad: { t: 0 },
+            x: 0.05,
+            len: 1,
+            currentvalue: {
+                xanchor: 'right',
+                prefix: ' ',
+                font: {
+                    color: '#FFF',
+                    size: 40
+                }
+            },
+            transition: { duration: 500 },
+            // By default, animate commands are bound to the most recently animated frame:
+            steps: [{
+                label: 'Plot',
+                method: 'animate',
+                args: [['Plot'], {
+                    mode: 'immediate',
+                    frame: { redraw: false, duration: 500 },
+                    transition: { duration: 500 }
+                }]
+            }, {
+                label: 'Story',
+                method: 'animate',
+                args: [['Story'], {
+                    mode: 'immediate',
+                    frame: { redraw: false, duration: 500 },
+                    transition: { duration: 500 }
+                }]
+            }]
+        }],
+
+        annotations: [
+            {
+                x: .5,
+                y: -.52,
+                xref: 'paper',
+                yref: 'paper',
+                text: 'Move the slider between Plot and Story<br>to see the different event orders',
+                showarrow: false
+            }
+
+        ]
+    };
+
+
+    var config = {
+        responsive: true,
+        displayModeBar: false
+    }
+
+
+    Plotly.newPlot('plotchartAA', {
+        data: data,
+        layout: layout,
+        frames: frames,
+        config: config
+    });
+
+
+};
+
+
+
 function setupStickyfill() {
     d3.selectAll(".sticky").each(function () {
         Stickyfill.add(this);
